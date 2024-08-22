e0707f18661c320c7fd98396604154bbf0a9acfd
LeonardFLY <leonard.alvesm@outlook.com>
2024-08-22 14:00:11 -0300

    Rodando o back-end

--- new-express-project/api/index.js
+++ new-express-project/api/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send("Eee caraio"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
+// Exporte o app diretamente, sem `app.listen`
+// Vercel irá usar esse export como a função de entrada
 module.exports = app;
